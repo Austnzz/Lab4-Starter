e7c99de30048d12dffbeb142406929aa2ac12600
Austnzz <akc006@ucsd.edu>
2025-04-29 22:53:38 -0700

    Fixed buggy script in lab4

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,7 +7,7 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
+        let result = 0; //changing this to let since we font rlly use outside block and we do arithmetic
 
         result = num1 + num2;
 
@@ -27,12 +27,11 @@ function sumValues(num1, num2, add) {
 function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
-    let discountedPrice = 0
     for(let i = 0; i < length; i++) {
+        let discountedPrice = 0 //moved inside cause it was outside scope earlier
         discountedPrice += prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
-
     return discounted;
 }
 
